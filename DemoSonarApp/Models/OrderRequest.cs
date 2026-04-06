e63c5af0a63ca06319f1f121da53aa5dc2e3b046
nha-vq <nhavo@kms-technology.com>
2026-04-06 14:15:03 +0700

    create service

---
+++ DemoSonarApp/Models/OrderRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoSonarApp.Models
+{
+    public class OrderRequest
+    {
+        public List<decimal> Prices { get; set; }
+        public string CustomerType { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
---
+++ DemoSonarApp/Services/OrderService.cs
@@ -0,0 +1,31 @@
+﻿namespace DemoSonarApp.Services;
+
+public class OrderService
+{
+    public decimal CalculateTotal(List<decimal> prices, string customerType, decimal discount)
+    {
+        decimal total = 0;
+
+        foreach (var price in prices)
+        {
+            total += price;
+        }
+
+        if (customerType == "VIP")
+        {
+            total -= total * 0.2m;
+        }
+
+        if (discount > 0)
+        {
+            total -= discount;
+        }
+
+        if (customerType == "INTERNAL")
+        {
+            Console.WriteLine("Internal customer discount applied");
+        }
+
+        return total;
+    }
+}
